236eb4b19397a038b232d431d67c124d2eddb9c2
denver-nail <1842852353@qq.com>
2025-01-15 18:02:20 +0800

    处理部署后项目中的图片展示失败问题：主要是因为nextJS的图片优化需要配置域名

--- slack-clone-tutorial/next.config.mjs
+++ slack-clone-tutorial/next.config.mjs
@@ -2,10 +2,17 @@
 const nextConfig = {
   images: {
     remotePatterns: [
+      // 下面这个是开发时使用的域名
       {
         protocol: "https",
         //你的域名
         hostname: "nautical-peacock-147.convex.cloud",
+      },
+      //下面这个是部署时使用的域名
+      {
+        protocol: "https",
+        //你的域名
+        hostname: "outstanding-panther-38.convex.cloud",
       },
     ],
   },
